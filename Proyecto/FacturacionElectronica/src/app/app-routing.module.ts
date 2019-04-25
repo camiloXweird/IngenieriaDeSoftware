56d48f66bf13509714c79fcbe1f68c66b4f6fdb1
Daniela <danacepeda253@gmail.com>
2019-04-25 09:46:11 -0500

    Se implemento el login en la aplicación

--- Proyecto/FacturacionElectronica/src/app/app-routing.module.ts
+++ Proyecto/FacturacionElectronica/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+import { AuthGuard } from './guards/auth.guard';
+import{NologinGuard} from "./guards/nologin.guard"
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren: './login/login.module#LoginPageModule', canActivate : [NologinGuard] },
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule',canActivate : [AuthGuard] }
+
 ];
 @NgModule({
   imports: [
--- Proyecto/FacturacionElectronica/src/app/tab3/tab3.page.ts
+++ Proyecto/FacturacionElectronica/src/app/tab3/tab3.page.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
-
+import { ActionSheetController } from '@ionic/angular';
+import{AuthService} from "../servicios/auth.service";
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {}
+export class Tab3Page {
+  constructor(public authservice : AuthService,public actionSheetController: ActionSheetController) {}
+  OnLogout(){
+    this.authservice.logout();
+  }
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Opciones',
+      buttons: [{
+        text: 'Cerrar Sesion',
+        role: 'destructive',
+        icon: 'log-out',
+        handler: () => {
+          this.OnLogout()
+        },
+      }]
+    });
+    await actionSheet.present();
+  }
+}
