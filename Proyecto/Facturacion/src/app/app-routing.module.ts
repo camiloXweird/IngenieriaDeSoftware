3bbcd09b281c3a57a6df5a43151e24f9d316aacb
camilo <cristianc@gmail.com>
2019-05-03 13:57:47 -0500

    Nuevo proyecto
    Un nuevo proyecto en blanco

---
+++ Proyecto/Facturacion/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'servicios', loadChildren: './servicios/servicios.module#ServiciosPageModule' },
+  { path: 'guards', loadChildren: './guards/guards.module#GuardsPageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
