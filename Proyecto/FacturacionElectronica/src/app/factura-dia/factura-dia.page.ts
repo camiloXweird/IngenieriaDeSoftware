ab676ed5d7d922bf64106cca428bad6be606f541
Cristian Triana <39069895+imuser97@users.noreply.github.com>
2019-05-22 13:14:27 -0500

    correcion del boton atras de facturas/dia
    Corregi el error el que hacia que cuando uno entraba a facturas/dia y le daba atras siempre enviaba a cajero

--- Proyecto/FacturacionElectronica/src/app/factura-dia/factura-dia.page.ts
+++ Proyecto/FacturacionElectronica/src/app/factura-dia/factura-dia.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActionSheetController } from '@ionic/angular';
-import{AuthService} from "../servicios/auth.service";
+import { AuthService } from "../servicios/auth.service";
 import { Router } from "@angular/router";
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-factura-dia',
@@ -10,14 +11,20 @@ import { Router } from "@angular/router";
 })
 export class FacturaDiaPage implements OnInit {
 
-  constructor(public authservice : AuthService,public actionSheetController: ActionSheetController
-    ,public router: Router) {}
+  constructor(public authservice: AuthService, public actionSheetController: ActionSheetController
+    , public router: Router) { }
 
   ngOnInit() {
   }
 
-  atras(){
-    this.router.navigate(['/inicio-cajero']);
+  atras() {
+    if (firebase.auth().currentUser.uid == "K4hqU8cheFfVcSWQvygD99TQPQ23") {
+      this.router.navigate(['/administrador'])
+      return false;
+    } if (firebase.auth().currentUser.uid == "mxaElOHrfzYw0wchY07sm7Gw5FR2") {
+      this.router.navigate(['/inicio-cajero'])
+      return false;
+    }
   }
 
 }
