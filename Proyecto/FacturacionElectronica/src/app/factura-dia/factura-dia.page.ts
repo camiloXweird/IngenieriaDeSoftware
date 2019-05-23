19725e0ef29a40a9b5574103b6c4429b29d54f2f
Cristian Triana <39069895+imuser97@users.noreply.github.com>
2019-05-23 18:12:48 -0500

    Update factura-dia.page.ts

--- Proyecto/FacturacionElectronica/src/app/factura-dia/factura-dia.page.ts
+++ Proyecto/FacturacionElectronica/src/app/factura-dia/factura-dia.page.ts
@@ -13,7 +13,7 @@ import { FacturaPage } from '../factura/factura.page';
 })
 export class FacturaDiaPage implements OnInit {
 
-  constructor(public navCtrl: NavController,public authservice: AuthService, public actionSheetController: ActionSheetController
+  constructor(public navCtrl: NavController, public authservice: AuthService, public actionSheetController: ActionSheetController
     , public router: Router) { }
 
   db = firebase.firestore();
@@ -36,28 +36,62 @@ export class FacturaDiaPage implements OnInit {
     let dia = new Date().getDay();
     let mes = new Date().getMonth();
     let anno = new Date().getFullYear();
-    this.db.collection(""+anno).doc(""+mes).collection(""+dia).get().then(function (querySnapshot) {
+    this.db.collection("" + anno).doc("" + mes).collection("" + dia).get().then(function (querySnapshot) {
       querySnapshot.forEach(function (doc) {
-        let item = document.createElement('ion-item');
         let label = document.createElement('ion-input');
         let button = document.createElement('ion-button');
         let textoLabel = document.createTextNode(doc.id);
         let textoButton = document.createTextNode('ver factura');
         label.disabled = true;
         label.appendChild(textoLabel);
-        item.appendChild(label);
-        button.click = this.mostrarFactura();
         button.appendChild(textoButton);
-        item.appendChild(button);
-          document.getElementById("facturas").appendChild(item);
-          //console.log(doc.id, " => ", doc.data());
-        });
+        document.getElementById("facturas").appendChild(label);
+        document.getElementById("facturas").appendChild(button);
+        button.onclick = ()=>{
+          let lista = document.createElement('ion-list');
+          let labelf = document.createElement('ion-label');
+          let labelf1 = document.createElement('ion-label');
+          let labelf2 = document.createElement('ion-label');
+          let labelf3 = document.createElement('ion-label');
+          let labelf4 = document.createElement('ion-label');
+          let labelf5 = document.createElement('ion-label');
+
+          let br = document.createElement('br');
+          lista.appendChild(labelf);
+          lista.appendChild(labelf1);
+          lista.appendChild(labelf2);
+          lista.appendChild(labelf3);
+          lista.appendChild(labelf4);
+          lista.appendChild(labelf5);
+          let textolabelf = document.createTextNode('Nombre del cliente: '+doc.data().nombre);
+          let textolabelf1 = document.createTextNode('Telefono del cliente: '+doc.data().telefono);
+          let textolabelf2 = document.createTextNode('Correo del cliente: '+doc.data().correo);
+          let textolabelf3 = document.createTextNode('Cantidad de productos comprados: '+doc.data().cantidad);
+          let textolabelf4 = document.createTextNode('Codigos de los productos: '+doc.data().codigo_producto);
+          let textolabelf5 = document.createTextNode('Valor de la compra: '+doc.data().valor_compra);
+          labelf.appendChild(textolabelf);
+          labelf.appendChild(br);
+          labelf1.appendChild(textolabelf1);
+          lista.appendChild(br);
+          labelf2.appendChild(textolabelf2);
+          lista.appendChild(br);
+          labelf3.appendChild(textolabelf3);
+          lista.appendChild(br);
+          labelf4.appendChild(textolabelf4);
+          lista.appendChild(br);
+          labelf5.appendChild(textolabelf5);
+          
+          document.getElementById("facturas").appendChild(lista);
+
+        }
+        //console.log(doc.id, " => ", doc.data());
+      });
     }).catch((error) => {
       console.log("error", error);
     });
   }
 
-  mostrarFactura(){
+  mostrarFactura() {
     console.log("ok");
   }
 
