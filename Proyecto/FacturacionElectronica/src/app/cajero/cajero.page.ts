da5ada764b43ef0231ce7b5723105ef24a91d4d1
Cristian Triana <39069895+imuser97@users.noreply.github.com>
2019-05-31 11:54:29 -0500

    mejoras en cajero

--- Proyecto/FacturacionElectronica/src/app/cajero/cajero.page.ts
+++ Proyecto/FacturacionElectronica/src/app/cajero/cajero.page.ts
@@ -3,6 +3,7 @@ import { ActionSheetController } from '@ionic/angular';
 import { AuthService } from "../servicios/auth.service";
 import { Router } from "@angular/router";
 import * as firebase from 'firebase';
+import { Button } from 'protractor';
 
 @Component({
   selector: 'app-cajero',
@@ -16,7 +17,7 @@ export class CajeroPage implements OnInit {
   db = firebase.firestore();
   factura: string;
   codigo: string;
-  cant: number;
+  cant: number = 0;
   valor: number;
   fecha_dia: string;
   fecha_mes: string;
@@ -24,6 +25,10 @@ export class CajeroPage implements OnInit {
   nombre: string;
   telefono: string;
   correo: string;
+
+  valorTotal: number = 0;
+  producto = document.getElementById('productos');
+  cantidad = document.getElementById('cantidad');
 
   ngOnInit() {
     this.productos();
@@ -55,31 +60,79 @@ export class CajeroPage implements OnInit {
     this.db.collection(this.fecha_anno).doc(this.fecha_mes).collection(this.fecha_dia).doc(this.factura).set({
       codigo_producto: this.codigo,
       cantidad: this.cant,
-      valor_compra: this.valor,
+      valor_compra: this.valorTotal,
       nombre: this.nombre,
       telefono: this.telefono,
       correo: this.correo,
-      fecha: this.fecha_anno+"/"+this.fecha_mes+"/"+this.fecha_dia,
+      fecha: this.fecha_anno + "/" + this.fecha_mes + "/" + this.fecha_dia,
       estado: 'Valida'
     }).then((datos) => {
       console.log(datos)
     }).catch((error) => {
       console.log(error);
     });
+    /*this.factura = '';
+    this.codigo = '';
+    this.cant=null;
+    this.valor=null;
+    this.fecha_dia='';
+    this.fecha_mes='';
+    this.fecha_anno='';
+    this.nombre='';
+    this.telefono='';
+    this.correo='';
+    this.valorTotal=null;*/
   }
 
-  productos(){
+  productos() {
     let productos = document.getElementById('productos');
-    this.db.collection('productos').get().then((querySnapshot)=>{
-      querySnapshot.forEach((doc)=>{
+    this.db.collection('productos').get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
         let select_option = document.createElement('ion-select-option');
-        let textSelect = document.createTextNode(doc.id+' - '+doc.data().nombre);
+        let textSelect = document.createTextNode(doc.data().nombre + ' - $' + doc.data().valor);
         select_option.value = doc.id;
         productos.appendChild(select_option);
         select_option.appendChild(textSelect);
       });
     });
   };
+
+  customAlertOptions: any = {
+    header: 'Productos',
+    subHeader: 'Selecciona un producto',
+    translucent: true
+  };
+
+
+  agregar() {
+    if (this.codigo != '') {
+      if (this.cant == 0) {
+        this.cant = 1;
+      }
+      let tablaCompra = document.getElementById('tableCompra');
+      let row = document.createElement('ion-row');
+      let colnombre = document.createElement('ion-col');
+      let colcantidad = document.createElement('ion-col');
+      let colvalor = document.createElement('ion-col');
+      let compra = this.cant;
+      row.appendChild(colnombre);
+      row.appendChild(colcantidad);
+      row.appendChild(colvalor);
+      let valorCompra: number;
+      this.db.collection('productos').doc(this.codigo).get().then((doc) => {
+        let valor = doc.get('valor');
+        tablaCompra.appendChild(row);
+        let textColNom = document.createTextNode(doc.data().nombre);
+        let textColCant = document.createTextNode(this.cant + '');
+        valorCompra = valor * compra;
+        let textColVal = document.createTextNode(valorCompra + '');
+        colnombre.appendChild(textColNom);
+        colcantidad.appendChild(textColCant);
+        colvalor.appendChild(textColVal);
+        this.valorTotal = this.valorTotal + valorCompra;
+      });
+    }
+  }
 
 }
 
